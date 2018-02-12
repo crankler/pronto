26f4fefec880f72222b1bf73bbdad5e1d9064c14
Marco Camurri <marco.camurri@iit.it>
2018-02-12 19:42:24 +0100

    adding fovis handler and lite version of the kalman filter

--- state-estimator/src/mav_state_est/sensor_handlers.hpp
+++ state-estimator/src/mav_state_est/sensor_handlers.hpp
@@ -14,9 +14,6 @@
 #include <lcmtypes/pronto/indexed_measurement_t.hpp>
 #include <lcmtypes/bot_core/kvh_raw_imu_batch_t.hpp>
 
-#include <estimate_tools/imu_stream.hpp>
-#include <estimate_tools/iir_notch.hpp>
-
 namespace MavStateEst {
 
 class InsHandler {
@@ -33,21 +30,6 @@ protected:
   RBISUpdateInterface * processMessage(const bot_core::ins_t * msg, MavStateEstimator* state_estimator);
   bool processMessageInit(const bot_core::ins_t * msg, const std::map<std::string, bool> & sensors_initialized
       , const RBIS & default_state, const RBIM & default_cov, RBIS & init_state, RBIM & init_cov);
-
-  // Compariable Atlas Functions:
-  RBISUpdateInterface * processMessageAtlas(const bot_core::kvh_raw_imu_batch_t * msg, MavStateEstimator* state_estimator);
-  bool processMessageInitAtlas(const bot_core::kvh_raw_imu_batch_t * msg, const std::map<std::string, bool> & sensors_initialized
-      , const RBIS & default_state, const RBIM & default_cov, RBIS & init_state, RBIM & init_cov);
-  //////////// Members Particular to Atlas:
-  double prev_utime_atlas; // cached previous time
-  IMUStream imu_data_;
-  void doFilter(IMUPacket &raw);
-  // An cascade of 3 notch filters in xyz
-  IIRNotch* notchfilter_x[3];
-  IIRNotch* notchfilter_y[3];
-  IIRNotch* notchfilter_z[3];
-  bool atlas_filter;
-  ////////////
 
   // Common Initialization Function:
   bool processMessageInitCommon(const std::map<std::string, bool> & sensors_initialized
